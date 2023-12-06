92ea8695b67c423a617819feb0d63aa7f450084b
J3SSY-ANDU <jessy.andujar@upr.edu>
2023-12-06 19:56:22 -0400

    multiple cookie values

--- src/main.js
+++ src/main.js
@@ -4,17 +4,33 @@ const btn = document.getElementById("btn");
 
 const audio = new Audio("../data/pop.mp3");
 
+function setCookies(cname, cvalue) {
+    cname = cname + "=";
+    cvalue = cvalue + "; ";
+    document.cookie = cname + cvalue;
+}
+
+let cookieCount = 0;
+let cookie = document.cookie;
+console.log(cookie.search("n"));
+cookie.search("n") >= 0 ? setCookies("n", parseInt(cookie.at(cookie.search("n")+2))) : setCookies("n", cookieCount);
 function addTask() {
     if (input.value != "") {
+        // let date = new Date();
+        let cookie = document.cookie;
+        let task_count = parseInt(cookie.at(cookie.search("n")+2));
+        setCookies("n", parseInt(cookie.at(cookie.search("n")+2))+1);
+        setCookies(`task_${++task_count}`, input.value);
+
         let task = document.createElement("li");
         let checkBox = document.createElement("input");
         let label = document.createElement("label");
-        audio.volume = 1;
-        audio.play();
         task.className = "task";
         checkBox.type = "checkbox";
         checkBox.name, checkBox.id = "checkBox";
         label.for = "checkBox";
+        audio.volume = 1;
+        audio.play();
         label.append(input.value);
         task.append(checkBox);
         task.append(label);
@@ -24,10 +40,8 @@ function addTask() {
 }
 
 function removeTask(event) {
-    
     const clickedTask = event.target;
     if (clickedTask.tagName === "INPUT") {
-        console.log(clickedTask.tagName);
         let parent = clickedTask.parentNode;
         let list = parent.parentNode;
         list.removeChild(parent);
