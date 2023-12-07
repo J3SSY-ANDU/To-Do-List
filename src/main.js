f0b6966082b2d2192a45ee3f5b0ee78c93bd4936
Jessy Andujar Cruz <124212160+J3SSY-ANDU@users.noreply.github.com>
2023-12-06 23:55:37 -0400

    organize code & add expiring date to cookies

--- src/main.js
+++ src/main.js
@@ -1,26 +1,38 @@
+// html nodes and general variables
 const input = document.getElementById("new-task");
 const list = document.getElementById("task-list");
 const btn = document.getElementById("btn");
 
+
+// Data
 const audio = new Audio("../data/pop.mp3");
 
-function setCookies(cname, cvalue) {
-    cname = cname + "=";
-    cvalue = cvalue + "; ";
-    document.cookie = cname + cvalue;
-}
 
+// Cookie n value
 let cookieCount = 0;
 let cookie = document.cookie;
 console.log(cookie.search("n"));
-cookie.search("n") >= 0 ? setCookies("n", parseInt(cookie.at(cookie.search("n")+2))) : setCookies("n", cookieCount);
+cookie.search("n") >= 0 ? setCookies("n", parseInt(cookie.at(cookie.search("n")+2)), 1) : setCookies("n", cookieCount, 1);
+
+
+// Set cookie
+function setCookies(cname, cvalue, exdays) {
+    let date = new Date();
+    date.setTime(date.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + date.toUTCString();
+    cname = cname + "=";
+    cvalue = cvalue + "; ";
+    document.cookie = cname + cvalue + expires + ";path=/src; " + "SameSite=None; " + "Secure;";
+}
+
+
+// Add task
 function addTask() {
     if (input.value != "") {
-        // let date = new Date();
         let cookie = document.cookie;
         let task_count = parseInt(cookie.at(cookie.search("n")+2));
-        setCookies("n", parseInt(cookie.at(cookie.search("n")+2))+1);
-        setCookies(`task_${++task_count}`, input.value);
+        setCookies("n", parseInt(cookie.at(cookie.search("n")+2))+1, 1);
+        setCookies(`task_${++task_count}`, input.value, 1);
 
         let task = document.createElement("li");
         let checkBox = document.createElement("input");
@@ -39,6 +51,8 @@ function addTask() {
     }
 }
 
+
+// Remove Task
 function removeTask(event) {
     const clickedTask = event.target;
     if (clickedTask.tagName === "INPUT") {
@@ -49,6 +63,7 @@ function removeTask(event) {
 }
 
 
+// Button events
 btn.addEventListener("click", addTask);
 input.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
@@ -56,4 +71,6 @@ input.addEventListener("keypress", (e) => {
     }
 });
 
+
+// Checkbox event
 list.addEventListener("change", removeTask);
